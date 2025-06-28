e3cdf653d3f65731d999b91b464ee4414205a306
Bennett Rennier <bennett@brennier.com>
2025-06-28 18:29:10 +0900

    Add isometric transformation

--- main.c
+++ main.c
@@ -3,14 +3,31 @@
 
 #define SCREENHEIGHT 1080
 #define SCREENWIDTH  1920
-#define GRIDSIZE 10
+#define GRIDSIZE 20
 #define TILESIZE 64
+
+// Green color palatte from lighest to darkest
+#define C1 (Color){ 138, 189, 76 }
+#define C2 (Color){ 64, 133, 109 }
+#define C3 (Color){ 48, 102, 87 }
+#define C4 (Color){ 36, 76, 64 }
+
+Vector2 IsoTransform(Vector2 coordinate) {
+    float x = 0.5 * coordinate.x - 0.5 * coordinate.y;
+    float y = 0.25 * coordinate.x + 0.25 * coordinate.y;
+    x += SCREENWIDTH / 2;
+    y += SCREENHEIGHT / 4;
+    return (Vector2){ x, y };
+}
 
 void DrawTiles() {
     Texture2D basic_tile = LoadTexture("assets/basic_tile.png");
     for (int i = 0; i < GRIDSIZE; i++)
-        for (int j = 0; j < GRIDSIZE; j++)
-            DrawTexture(basic_tile, TILESIZE * j, TILESIZE * i, WHITE);
+        for (int j = 0; j < GRIDSIZE; j++) {
+            Vector2 origin = { TILESIZE * j, TILESIZE * i };
+            origin = IsoTransform(origin);
+            DrawTextureV(basic_tile, origin, WHITE);
+        }
 }
 
 int main() {
@@ -24,7 +41,7 @@ int main() {
     {
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        ClearBackground(C1);
 
         if (IsKeyDown(KEY_RIGHT))
             x++;
