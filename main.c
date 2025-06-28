093644aa9e9dfa57338a0ca1b36a85322ee9bdbf
Bennett Rennier <bennett@brennier.com>
2025-06-28 20:44:27 +0900

    Add Gameboy border and only load textures once

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include "include/raylib.h"
 #include "include/raymath.h"
 #include <math.h>
-
-#define SCREENHEIGHT 1080
-#define SCREENWIDTH  1920
+#define SCREENHEIGHT 930
+#define SCREENWIDTH  1120
 #define GRIDSIZE 20
 #define TILESIZE 64
 
@@ -14,6 +13,7 @@
 #define C4 (Color){ 36, 76, 64 }
 
 float tile_height[GRIDSIZE][GRIDSIZE] = { 0 };
+Vector2 tile_offset = { 0 };
 
 Vector2 IsoTransform(Vector2 coordinate) {
     float x = 0.5 * coordinate.x - 0.5 * coordinate.y;
@@ -24,30 +24,47 @@ Vector2 IsoTransform(Vector2 coordinate) {
     return (Vector2){ x, y };
 }
 
-void DrawTiles() {
-    Texture2D basic_tile = LoadTexture("assets/basic_tile.png");
+void DrawTiles(Texture2D basic_tile) {
     for (int i = 0; i < GRIDSIZE; i++)
         for (int j = 0; j < GRIDSIZE; j++) {
             Vector2 origin = { TILESIZE * j, TILESIZE * i };
             origin = IsoTransform(origin);
             // Raise blocks according to their height
             origin.y -= tile_height[j][i] * TILESIZE * 0.5;
-            DrawTextureV(basic_tile, origin, WHITE);
+            DrawTextureV(basic_tile, Vector2Add(origin, tile_offset), WHITE);
         }
 }
 
 int main() {
     InitWindow(SCREENWIDTH, SCREENHEIGHT, "Basic ball program");
+    Texture2D border = LoadTexture("assets/gameboy_border.png");
+    Texture2D basic_tile = LoadTexture("assets/basic_tile.png");
     SetTargetFPS(60);
 
     while (WindowShouldClose() != true) {
         for (int i = 0; i < GRIDSIZE; i++)
-            for (int j = 0; j < GRIDSIZE; j++)
-                tile_height[j][i] = 0.25 * sin(3 * GetTime() + (j + i));
+            for (int j = 0; j < GRIDSIZE; j++) {
+                tile_height[j][i] =  sin(M_PI * GetTime());
+                tile_height[j][i] += sin(M_PI * GetTime() + i);
+                tile_height[j][i] += sin(M_PI * GetTime() + j);
+                tile_height[j][i] *= 0.25;
+            }
+
+        if (IsKeyDown(KEY_RIGHT))
+            tile_offset.x -= 3;
+        if (IsKeyDown(KEY_LEFT))
+            tile_offset.x += 3;
+        if (IsKeyDown(KEY_DOWN))
+            tile_offset.y -= 3;
+        if (IsKeyDown(KEY_UP))
+            tile_offset.y += 3;
+
+
 
         BeginDrawing();
         ClearBackground(C1);
-        DrawTiles();
+        DrawTiles(basic_tile);
+        DrawTexture(border, 0, 0, WHITE);
         EndDrawing();
     }
 
