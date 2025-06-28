33153472803719a4d253c3591b3698a876e53693
Bennett Rennier <bennett@brennier.com>
2025-06-28 17:22:20 +0900

    Display a single tile

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include "include/raylib.h"
 #include "include/raymath.h"
 
-#define SCREENHEIGHT 480
-#define SCREENWIDTH  720
+#define SCREENHEIGHT 1080
+#define SCREENWIDTH  1920
 
 int main() {
     InitWindow(SCREENWIDTH, SCREENHEIGHT, "Basic ball program");
     SetTargetFPS(60);
 
+    Texture2D basic_tile = LoadTexture("assets/basic_tile.png");
+
+    int x = 10;
+
     while (WindowShouldClose() != true)
     {
         BeginDrawing();
 
-        DrawRectangle(10, 10, 100, 100, RED);
+        ClearBackground(BLACK);
+
+        if (IsKeyDown(KEY_RIGHT))
+            x++;
+
+        DrawTexture(basic_tile, x, 10, WHITE);
 
         EndDrawing();
     }
